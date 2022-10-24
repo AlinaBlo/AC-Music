03bb06442c51b3b1963c43deba598f4b3117890e
AlinaBlo <82020749+AlinaBlo@users.noreply.github.com>
2022-10-24 13:37:13 +0300

    Add files via upload

---
+++ MusicalInst.js
@@ -0,0 +1,160 @@
+﻿musicalInstruments = [
+    {//1
+        "name":"Roland VAD506 V-Drum",
+        "short_description": "The Ultimate V-Drums Acoustic Design Experience.",
+        "price": "5,000",
+        "image": "https://static.keymusic.com/products/287848/340/roland-vad506-v-drum.jpg",
+        "amount": "10",
+        "category": "Drums",
+        "check_in_stock": "01.02.2020",
+        "manufacturer": "Roland",
+        "tags": [
+            {"material": "wood shells"},
+            { "Importer_country": "germany" }
+        ]
+    },
+    {//2
+        "name": "Pearl Decade Maple Series dmp905pc",
+        "short_description": "With 5.4mm, all Maple shells, fully adjustable pro level features, and gorgeous lacquer finishes, Decade elevates your performance at a price that was previously impossible. ",
+        "price": "9,000",
+        "image": "https://kolhamusica.com/wp-content/uploads/88_DMP905PC_225-1.jpg",
+        "amount": "5",
+        "category": "Drums",
+        "check_in_stock": "01.02.2020",
+        "manufacturer": "Pearl ",
+        "tags": [
+            
+                { "material": "Maple Shells" },
+                { "Importer_country": "greece" }
+            
+        ]
+    },
+    {//3
+        "name": "KAWAI NOVUS NV10",
+        "short_description": "A celebration of Kawai’s 90 years of piano innovation, the NOVUS NV10 Hybrid encapsulates the company’s many acoustic and digital achievements, fusing tradition with technology to redefine the grand piano experience for players and audience members alike.",
+        "price": "30,000",
+        "image": "https://kawaius.com/wp-content/uploads/2018/04/Kawai-Novus-NV10.jpg",
+        "amount": "4",
+        "category": "piano",
+        "check_in_stock": "18.02.2021",
+        "manufacturer": "KAWAI",
+        "tags": [
+            
+                { "material": "Wooden" },
+                { "Importer_country": "greece" }
+            
+        ]
+    },
+    {//4
+        "name": "Roland VR-730 Live Performance Keyboard",
+        "short_description": "The V-Combo VR-730 delivers top-quality organ, electric piano, acoustic piano, and synth sounds in a travel-ready instrument with a 73-note waterfall keyboard.",
+        "price": "1,500",
+        "image": "https://static.keymusic.com/products/258338/XL/roland-vr-730-live-performance-keyboard-4.jpg",
+        "amount": "15",
+        "category": "piano",
+        "check_in_stock": "07.08.2020",
+        "manufacturer": "Roland",
+        "tags": [
+            
+                { "material": "metal"},
+                { "Importer_country": "israel" }
+            
+        ]
+    },
+    {//5
+        "name": "PRIMERA 4/4 CLASSICAL GUITAR PRODIPE GUITARS JMFPRIMERA4/4",
+        "short_description": "Primera 4/4 Classical guitar Prodipe Guitars JMFPRIMERA4/4. From design to manufacturing to wood essences",
+        "price": "1,400",
+        "image": "https://www.stgd.ch/1662-large_default/primera-44-classical-guitar-prodipe-guitars-jmfprimera44.jpg",
+        "amount": "7",
+        "category": "guitar",
+        "check_in_stock": "14.05.2020",
+        "manufacturer": "PRIMERA",
+        "tags": [
+            
+                { "material": "wood" },
+            { "Importer_country": "china" }
+            
+        ]
+    },
+    {//6
+        "name": "E6BLK Encore",
+        "short_description": "Finished in gloss black",
+        "price": "700",
+        "image": "https://d3m9l0v76dty0.cloudfront.net/system/photos/4434702/original/1fa0b52e1965878a414816ecea40111c.jpg",
+        "amount": "8",
+        "category": "guitar",
+        "check_in_stock": "14.05.2020",
+        "manufacturer": "Encore",
+        "tags": [
+            
+            { "material": "wood" },
+            { "Importer_country": "china" }
+            
+        ]
+    },
+    {//7
+        "name": "4 String Banjo",
+        "short_description": "Remo Skin banjo head - for a resonant tone and increased volume",
+        "price": "300",
+        "image": "https://d1aeri3ty3izns.cloudfront.net/media/54/543065/600/preview.jpg",
+        "amount": "2",
+        "category": "banjo",
+        "check_in_stock": "07.08.2019",
+        "manufacturer": "Gear4music",
+        "tags": [
+            
+            { "material": "aluminium" },
+            { "Importer_country": "israel" }
+            
+        ]
+    },
+    {//8
+        "name": "Jupiter JP-767 GL-Q Eb-Alto Saxophone",
+        "short_description": "The Jupiter JP-767 GL-Q Alto Saxophone is the saxophone for ambitious students for the new generation.",
+        "price": "7,000",
+        "image": "https://images.musicstore.de/images/1280/jupiter-jp-767-gl-q-eb-alto-saxophone_1_BLA0003900-000.jpg.webp",
+        "amount": "3",
+        "category": "saxophone",
+        "check_in_stock": "24.11.2020",
+        "manufacturer": "Jupiter",
+        "tags": [
+            
+            { "material": "Brass" },
+            { "Importer_country": "Bulgaria" }
+            
+        ]
+    },
+    {//9
+        "name": "Chinese Gong",
+        "short_description": "A simple, traditional, hand-manufactured Feng gong from Wuhan",
+        "price": "200",
+        "image": "https://media.manufactum.de/is/image/Manufactum/750s_shop/chinese-gong--71343_01.jpg",
+        "amount": "6",
+        "category": "Drums",
+        "check_in_stock": "23.03.2020",
+        "manufacturer": "Gong",
+        "tags": [
+            
+                { "material": "steel" },
+                { "Importer_country": "china" }
+            
+        ]
+    },
+    {//10
+        "name": "Vic Firth American Classic 5A Hickory Drumsticks, Wood Tip",
+        "short_description": "The drummer's go-to drumstick choice",
+        "price": "50",
+        "image": "https://d1aeri3ty3izns.cloudfront.net/media/39/395750/600/preview_1.jpg",
+        "amount": "10",
+        "category": "Drums",
+        "check_in_stock": "02.08.2020",
+        "manufacturer": "Vic Firth",
+        "tags": [
+            
+                { "material": "wood" },
+                { "Importer_country": "america" }
+            
+        ]
+    }
+]
